919a956a70dd493f20e4de12018c8c5b96673229
Icily57 <icilyweird57@gmail.com>
2025-02-21 16:02:12 +0300

    moving on

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'drizzle-kit';
+import 'dotenv/config';
+
+
+export default defineConfig({
+    dialect: 'postgresql',
+    schema: './src/drizzle/schema.ts',
+    out: './src/drizzle/migrations',
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
+    verbose: true,
+    strict: true,
+});
